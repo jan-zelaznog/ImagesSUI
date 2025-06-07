dd36e42f604718ed8ea398cb9479b53013d83d9c
jan-zelaznog <jan.zelaznog@gmail.com>
2025-06-06 22:08:47 -0600

    Initial Commit

---
+++ ImagesSUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImagesSUI
+//
+//  Created by Ángel González on 06/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImagesSUI/ImagesSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagesSUIApp.swift
+//  ImagesSUI
+//
+//  Created by Ángel González on 06/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ImagesSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
