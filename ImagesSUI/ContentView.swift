b8b6e928e1d0a5af33678507ac6e75c218021304
jan-zelaznog <jan.zelaznog@gmail.com>
2025-06-07 09:03:07 -0600

    INITIAL COMMIT

--- ImagesSUI/ContentView.swift
+++ ImagesSUI/ContentView.swift
@@ -8,15 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var showPicker = false
+    @State private var showCamera = false
+    @State private var showSourceChoice = false
+    @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    @State private var image: UIImage?
+
+        var body: some View {
+            VStack {
+                if let image = image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 300)
+                } else {
+                    Text("No se ha tomado ninguna foto")
+                        .foregroundColor(.cyan)
+                }
+
+                Button("Tomar Foto") {
+                    showSourceChoice = true
+                }
+                .padding()
+                .confirmationDialog("Selecciona una fuente", isPresented: $showSourceChoice) {
+                                Button("Cámara") {
+                                    sourceType = .camera
+                                    showPicker = true
+                                }
+                                Button("Galería") {
+                                    sourceType = .photoLibrary
+                                    showPicker = true
+                                }
+                                Button("Cancelar", role: .cancel) {}
+                            }
+                            .sheet(isPresented: $showPicker) {
+                                ImagePicker(sourceType: sourceType, selectedImage: $image)
+                            }
+            }
+            .padding()
         }
-        .padding()
-    }
 }
 
 #Preview {
